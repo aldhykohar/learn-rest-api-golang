2178bbc592d0ad2a26c2fd006f02c4fa759a6b3c
aldhykohar <aldhykohar@stimednp.ac.id>
2021-10-30 21:25:03 +0800

    :sparkles: preparing authcontroller

--- server.go
+++ server.go
@@ -1,13 +1,26 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"golang_rest_api/config"
+	"golang_rest_api/controller"
+
+	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
+)
+
+var (
+	db             *gorm.DB                  = config.SetupDatabaseConnection()
+	authController controller.AuthController = controller.NewAuthController()
+)
 
 func main() {
+	defer config.CloseDatabaseConnection(db)
 	r := gin.Default()
-	r.GET("/", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "hello world",
-		})
-	})
+
+	authRoutes := r.Group("api/auth")
+	{
+		authRoutes.POST("/login", authController.Login)
+		authRoutes.POST("/register", authController.Register)
+	}
 	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
