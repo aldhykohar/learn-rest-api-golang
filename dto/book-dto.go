0a13374b0717020fc5962d3112f6a853e4223b72
aldhykohar <aldhykohar@stimednp.ac.id>
2021-10-31 13:29:28 +0800

    :sparkles: setup user repository

--- dto/book-dto.go
+++ dto/book-dto.go
@@ -1,6 +1,16 @@
 package dto
 
+// BookUpdateDTO is model that client use when update book
 type BookUpdateDTO struct {
-	ID    uint64 `json:"id" form:"id" binding:"required"`
-	Title string `json:"title" form:"title" binding:"required"`
+	ID          uint64 `json:"id" form:"id" binding:"required"`
+	Title       string `json:"title" form:"title" binding:"required"`
+	Description string `json:"description" form:"description" binding:"required"`
+	UserID      uint64 `json:"user_id,omitempty" form:"user_id,omitempty" `
+}
+
+// BookCreateDTO is model that client use when create a new book
+type BookCreateDTO struct {
+	Title       string `json:"title" form:"title" binding:"required"`
+	Description string `json:"description" form:"description" binding:"required"`
+	UserID      uint64 `json:"user_id,omitempty" form:"user_id,omitempty" `
 }
--- dto/register-dto.go
+++ dto/register-dto.go
@@ -1,5 +1,6 @@
 package dto
 
+// RegisterDTO is used when client post form /register url
 type RegisterDTO struct {
 	Name     string `json:"name" form:"name" binding:"required" validate:"min:1"`
 	Email    string `json:"email" form:"email" binding:"required" validate:"email"`
